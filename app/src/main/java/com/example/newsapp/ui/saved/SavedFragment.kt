f8bf07b216a918b65cabce50ef42be43055adb47
SeongHo Park <42669772+psh2849@users.noreply.github.com>
2022-09-08 03:08:57 +0900

    Add Application & Hilt Annotations

---
+++ app/src/main/java/com/example/newsapp/ui/saved/SavedFragment.kt
@@ -0,0 +1,20 @@
+package com.example.newsapp.ui.saved
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.newsapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SavedFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_saved, container, false)
+    }
+}
