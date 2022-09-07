ea8c1a7eb6600d6b4485b8f67a3818ea7464660c
SeongHo Park <42669772+psh2849@users.noreply.github.com>
2022-09-07 23:00:19 +0900

    Feat: Develop BottomNavigationView

---
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.newsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.newsapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setUpNavController()
+    }
+
+    private fun setUpNavController() {
+        navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.topNewsFragment,
+            R.id.categoriesFragment,
+            R.id.savedFragment
+        ))
+        binding.bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
