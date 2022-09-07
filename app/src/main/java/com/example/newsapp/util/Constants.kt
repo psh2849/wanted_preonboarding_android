5da9b23643f11b5398dc66cc9c8b3f2da6286e0c
SeongHo Park <42669772+psh2849@users.noreply.github.com>
2022-09-08 03:04:36 +0900

    Add Constants, NetworkResult, DiffUtil Util Files

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "3e41ea6dbe1c4dac8493476de2f17906"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
