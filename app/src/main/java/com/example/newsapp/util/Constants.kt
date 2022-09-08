11282545458319ac1c18b47eabedfe13de46ab84
SeongHo Park <42669772+psh2849@users.noreply.github.com>
2022-09-09 01:20:16 +0900

    Add Constants

--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -2,8 +2,17 @@ package com.example.newsapp.util
 
 class Constants {
     companion object {
-        const val API_KEY = "3e41ea6dbe1c4dac8493476de2f17906"
+
         const val BASE_URL = "https://newsapi.org"
-        const val NEWS_BUNDLE = "NEWS_RESULT"
+
+        const val NEWS_COUNTRY = "country"
+        const val NEWS_APIKEY = "apiKey"
+        const val NEWS_PAGE_SIZE = "pageSize"
+        const val NEWS_CATEGORY = "category"
+
+        const val INPUT_NEWS_COUNTRY = "us"
+        const val INPUT_NEWS_API_KEY = ""
+        const val INPUT_NEWS_PAGE_SIZE = "100"
+
     }
 }
