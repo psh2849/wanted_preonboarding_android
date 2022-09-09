15d3d2bb41e01e6cb6a580353970787b3022e77b
SeongHo Park <42669772+psh2849@users.noreply.github.com>
2022-09-09 19:31:49 +0900

    Feat: Develop Star Icon

--- app/src/main/java/com/example/newsapp/ui/NewsDetailFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/NewsDetailFragment.kt
@@ -1,19 +1,27 @@
 package com.example.newsapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import coil.load
 import com.example.newsapp.R
 import com.example.newsapp.bindingadapter.NewsRowBinding
+import com.example.newsapp.data.database.entity.SavedEntity
+import com.example.newsapp.viewmodel.NewsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_news_detail.view.*
 
 @AndroidEntryPoint
 class NewsDetailFragment : Fragment() {
+    private val newsViewModel : NewsViewModel by viewModels()
+    private var isNewsSaved = false
+    private var savedId = 0
     private val args by navArgs<NewsDetailFragmentArgs>()
 
     override fun onCreateView(
@@ -24,7 +32,46 @@ class NewsDetailFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_news_detail, container, false)
 
         getArgsFromTopNewsFragment(view)
+
+        view.iv_detail_star.setOnClickListener {
+            clickSavedStarButton(view)
+        }
+
         return view
+    }
+
+    private fun clickSavedStarButton(view: View) {
+        if(!isNewsSaved) {
+            saveToNews(view)
+        } else {
+            deleteToNews(view)
+        }
+    }
+
+    private fun saveToNews(view: View) {
+        val savesEntity = SavedEntity(
+            0,
+            args.article
+        )
+
+        newsViewModel.insertSavedNews(savesEntity)
+        view.iv_detail_star.setImageResource(R.drawable.ic_selected_star)
+        Toast.makeText(context, "Save News!", Toast.LENGTH_SHORT).show()
+        isNewsSaved = true
+    }
+
+
+
+    private fun deleteToNews(view: View) {
+        val savesEntity = SavedEntity(
+            savedId,
+            args.article
+        )
+
+        newsViewModel.deleteSavedNews(savesEntity)
+        view.iv_detail_star.setImageResource(R.drawable.ic_no_selected_star)
+        Toast.makeText(context, "Delete News!", Toast.LENGTH_SHORT).show()
+        isNewsSaved = false
     }
 
     private fun getArgsFromTopNewsFragment(view: View) {
@@ -48,5 +95,25 @@ class NewsDetailFragment : Fragment() {
             view.tv_detail_content.text = content
         }
 
+        checkSelectedStar(view)
+    }
+
+    private fun checkSelectedStar(view: View) {
+        newsViewModel.readSavedNews.observe(viewLifecycleOwner) { newsEntity ->
+            try {
+                for(savedNews in newsEntity) {
+                    if(savedNews.article.url == args.article.url) {
+                        view.iv_detail_star.setImageResource(R.drawable.ic_selected_star)
+                        savedId = savedNews.id
+                        isNewsSaved = true
+                        break
+                    } else {
+                        view.iv_detail_star.setImageResource(R.drawable.ic_no_selected_star)
+                    }
+                }
+            } catch (e: Exception) {
+                Log.d("NewsDetailFragment", e.message.toString())
+            }
+        }
     }
 }
--- app/src/main/java/com/example/newsapp/ui/saved/SavedFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/saved/SavedFragment.kt
@@ -5,16 +5,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapp.R
+import com.example.newsapp.adapter.CategoryAdapter
+import com.example.newsapp.adapter.SavedAdapter
+import com.example.newsapp.databinding.FragmentCategoriesListBinding
+import com.example.newsapp.databinding.FragmentSavedBinding
+import com.example.newsapp.model.NewsResult
+import com.example.newsapp.viewmodel.NewsViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SavedFragment : Fragment() {
+    private lateinit var newsViewModel: NewsViewModel
+
+    private var _binding: FragmentSavedBinding? = null
+    private val binding get() = _binding!!
+
+    private val mAdapter by lazy { SavedAdapter() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        newsViewModel = ViewModelProvider(requireActivity())[NewsViewModel::class.java]
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_saved, container, false)
+        _binding = FragmentSavedBinding.inflate(inflater, container, false)
+        setRecyclerView()
+
+        lifecycleScope.launch {
+            newsViewModel.readSavedNews.observe(viewLifecycleOwner) { saved ->
+                mAdapter.setData(saved)
+            }
+        }
+
+        return binding.root
+    }
+
+    private fun setRecyclerView() {
+
+        binding.rvSaved.adapter = mAdapter
+        binding.rvSaved.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
